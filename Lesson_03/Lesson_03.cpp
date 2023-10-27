d77dc9616b7818840f2eb279db14597a9c1f13e5
Андрей Морозов <andry.seredenin@yandex.ru>
2023-10-27 19:09:50 +0400

    Add lesson_03 and lesson_04

---
+++ Lesson_03/Lesson_03.cpp
@@ -0,0 +1,133 @@
+﻿// BinaryHeap.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class Heap {
+    static const int SIZE = 100; // максимальный размер кучи
+    int* h;         // указатель на массив кучи
+    int HeapSize; // размер кучи
+public:
+    Heap();  // конструктор кучи
+    void addelem(int);  // добавление элемента кучи
+    void outHeap();  // вывод элементов кучи в форме кучи
+    void out();  // вывод элементов кучи в форме массива
+    int getmax();  // удаление вершины (максимального элемента)
+    void heapify(int);  // упорядочение кучи
+};
+
+Heap::Heap() {
+    h = new int[SIZE];
+    HeapSize = 0;
+}
+
+void Heap::addelem(int n) {
+    int i, parent;
+    i = HeapSize;
+    h[i] = n;
+    parent = (i - 1) / 2;
+    while (parent >= 0 && i > 0) {
+        if (h[i] > h[parent]) {
+            int temp = h[i];
+            h[i] = h[parent];
+            h[parent] = temp;
+        }
+        i = parent;
+        parent = (i - 1) / 2;
+    }
+    HeapSize++;
+}
+
+void Heap::outHeap(void) {
+    int i = 0;
+    int k = 1;
+    while (i < HeapSize) {
+        while ((i < k) && (i < HeapSize)) {
+            cout << h[i] << " ";
+            i++;
+        }
+        cout << endl;
+        k = k * 2 + 1;
+    }
+}
+
+void Heap::out(void) {
+    for (int i = 0; i < HeapSize; i++) {
+        cout << h[i] << " ";
+    }
+    cout << endl;
+}
+
+void Heap::heapify(int i) {
+    int left, right;
+    int temp;
+    left = 2 * i + 1;
+    right = 2 * i + 2;
+    if (left < HeapSize) {
+        if (h[i] < h[left]) {
+            temp = h[i];
+            h[i] = h[left];
+            h[left] = temp;
+            heapify(left);
+        }
+    }
+    if (right < HeapSize) {
+        if (h[i] < h[right]) {
+            temp = h[i];
+            h[i] = h[right];
+            h[right] = temp;
+            heapify(right);
+        }
+    }
+}
+
+int Heap::getmax(void) {
+    int x;
+    x = h[0];
+    h[0] = h[HeapSize - 1];
+    HeapSize--;
+    heapify(0);
+    return(x);
+}
+
+
+
+int main()
+{
+    Heap heap;
+    int k;
+    system("chcp 1251");
+    system("cls");
+    for (int i = 0; i < 16; i++) {
+        cout << "Введите элемент " << i + 1 << ": ";
+        cin >> k;
+        heap.addelem(k);
+    }
+    heap.outHeap();
+    cout << endl;
+    heap.out();
+    cout << endl << "Максимальный элемент: " << heap.getmax();
+    cout << endl << "Новая куча: " << endl;
+    heap.outHeap();
+    cout << endl;
+    heap.out();
+    cout << endl << "Максимальный элемент: " << heap.getmax();
+    cout << endl << "Новая куча: " << endl;
+    heap.outHeap();
+    cout << endl;
+    heap.out();
+    cin.get(); cin.get();
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Lesson_04/Lesson_04.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+using namespace std;
+
+class iList
+{
+public:
+    iList() : Head(NULL), Tail(NULL) {}
+    void add(int value);
+    void show();
+    ~iList();
+
+private:
+    void sort();
+    struct Node
+    {
+        int x;
+        Node* Next, * Prev;
+    };
+
+    Node* Head, * Tail;
+};
+
+
+
+void iList::add(int value)
+{
+    Node* temp = new Node;
+    temp->x = value;
+    temp->Next = NULL;
+
+    if (!Head) {
+        temp->Prev = NULL;
+        Head = Tail = temp;
+    }
+    else {
+        temp->Prev = Tail;
+        Tail->Next = temp;
+        Tail = temp;
+    }
+    sort();
+}
+
+void iList::show()
+{
+    Node* temp = Head;
+    while (temp) {
+        cout << temp->x << '\t';
+        temp = temp->Next;
+    }
+}
+
+void iList::sort()
+{
+    Node* left = Head;
+    Node* right = Head->Next;
+
+    Node* temp = new Node;
+
+    while (left->Next) {
+        while (right) {
+            if ((left->x) < (right->x)) {
+                temp->x = left->x;
+                left->x = right->x;
+                right->x = temp->x;
+            }
+            right = right->Next;
+        }
+        left = left->Next;
+        right = left->Next;
+    }
+}
+
+iList::~iList()
+{
+    while (Head)
+    {
+        Tail = Head->Next;
+        delete Head;
+        Head = Tail;
+    }
+}
+
+int main()
+{
+    iList ilst;
+
+    while (true)
+    {
+        cout << "input number -> ";
+        int a;
+        cin >> a;
+        ilst.add(a);
+        ilst.show();
+        cout << "1-Add, 2-quick -> ";
+        cin >> a;
+        if (a == 2) break;
+
+    }
+}
+
